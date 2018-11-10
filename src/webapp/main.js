3c964a9ddb5fdcbe4c0517b9e8e6452a781e0380
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2018-11-10 17:19:11 +0100

    docker -- run webapp in debug mode with 2 way fs sync

--- src/webapp/main.js
+++ src/webapp/main.js
@@ -1,10 +1,24 @@
 const http = require("http");
+const fs = require("fs");
 
 const PORT = process.env.PORT || 8080;
+const FILEPATH = "./tmp/log.txt";
+
+let index = 0;
+
 const app = http.createServer((request, response) => {
+  index++;
+
+  const str = `Hello world ${index} times`;
+
+  // test two way fs synching
+  fs.writeFileSync(FILEPATH, str);
+
   response.writeHead(200, { "Content-Type": "text/html" });
-  response.write(`Hello world`);
+  response.write(str);
   response.end();
 });
 
 app.listen(PORT);
+
+console.log(`Server is running at port ${PORT}`);
