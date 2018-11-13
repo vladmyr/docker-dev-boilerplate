4c83bbf8f735b8beb9ba28bab8f58062c5c78481
volodymyr.khytskyi <volodymyr.khytskyi@vkhytskyi.local>
2018-11-13 14:52:10 +0100

    tests -- simplest healthcheck test

---
+++ tests/healthcheck/main.js
@@ -0,0 +1,18 @@
+const assert = require("assert");
+const puppeteer = require("puppeteer");
+
+const PORT = process.env.PORT || 8080;
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  try {
+    await page.goto(`http://127.0.0.1:${PORT}`);
+    assert.ok(1);
+    await browser.close();
+  } catch(e) {
+    await browser.close();
+    assert.fail(e);
+  }
+})();
